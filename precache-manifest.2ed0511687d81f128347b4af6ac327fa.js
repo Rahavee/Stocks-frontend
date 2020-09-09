57ea2778d51d3acc2b7892844031ad11022f1da9
Rahavee <rahaveerv@gmail.com>
2020-09-09 00:26:07 -0400

    Updates

---
+++ precache-manifest.2ed0511687d81f128347b4af6ac327fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d76e18118e81fb9c277aaeef90ae11",
+    "url": "/Stocks-frontend/index.html"
+  },
+  {
+    "revision": "cf1ea60756d71a691887",
+    "url": "/Stocks-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d7117a7c98f304e76382",
+    "url": "/Stocks-frontend/static/js/2.a9c84109.chunk.js"
+  },
+  {
+    "revision": "2dfeae871ed0df81b1a1830be7553a53",
+    "url": "/Stocks-frontend/static/js/2.a9c84109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1ea60756d71a691887",
+    "url": "/Stocks-frontend/static/js/main.6eed5df0.chunk.js"
+  },
+  {
+    "revision": "825a77857910976647f4",
+    "url": "/Stocks-frontend/static/js/runtime-main.88eac02b.js"
+  }
+]);
